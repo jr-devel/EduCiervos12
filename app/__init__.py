8115274c2f9de54bd21fc89b5adef1ecf3a379cb
jr-devel <34898338+jr-devel@users.noreply.github.com>
2022-10-07 12:43:48 -0500

    First Commit

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+import os
+from flask import *
+from .logger_base import log
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_mapping( 
+        SECRET_KEY        = 'RIEJ030619HNECSNA9',
+        DATABASE          = os.environ.get("FLASK_DATABASE"),
+        DATABASE_HOST     = os.environ.get("FLASK_DATABASE_HOST"),
+        DATABASE_USER     = os.environ.get("FLASK_DATABASE_USER"),
+        DATABASE_PASSWORD = os.environ.get("FLASK_DATABASE_PASSWORD")
+    )
+    #
+    from . import views
+    app.register_blueprint(views.bp)
+    #
+    return app
